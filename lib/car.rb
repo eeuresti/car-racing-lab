fcb69e0664afa0c04c7b0391fb8c80e5c9e96453
Eric Euresti <eeuresti@gmail.com>
2015-09-16 20:49:10 -0700

    works but intermitenly

--- lib/car.rb
+++ lib/car.rb
@@ -1,3 +1,21 @@
 class Car
-  #write Car class code here
+  attr_accessor :speed
+  def initialize
+    @speed = 0
+  end
+
+  def accelerate(speed)
+    @speed += speed
+  end
+
 end
+
+
+
+
+
+
+# a Car class that can accelerate to a certain speed
+# a Race class that when instantiated instantiates two cars and accelerates each to a random speed between 0-100
+# an instance method on the Race class called winner that returns the winning car (determined by a greater speed)
+# an instance method on the Race class called loser that returns the losing car (determined by a lesser speed)
--- lib/race.rb
+++ lib/race.rb
@@ -1,5 +1,42 @@
-require_relative 'car'
-
 class Race
-  #write Race class code here
+    attr_accessor :cars
+    def initialize
+        @cars = [Car.new, Car.new]
+
+        @speed1 = @cars[0].accelerate(rand(1..100))
+        @speed2 = @cars[1].accelerate(rand(1..100))
+
+    end
+    def winner
+      @winner = (@cars[0].accelerate(rand(1..100)) < @cars[1].accelerate(rand(1..100))) ? @cars[1]: @cars[0]
+    end
+    def loser
+        if @speed1 < @speed2
+        @loser = @cars[0]
+        else
+        @loser = @cars[1]
+        end
+    end
 end
+
+# require_relative 'car'
+#
+# class Race
+#   attr_accessor :cars
+#   def initialize
+#     @cars = [Car.new, Car.new]
+#
+#     @speed1 = @cars[0].accelerate(rand(100))
+#     @speed2 = @cars[1].accelerate(rand(100))
+#   end
+#
+#   def winner
+#
+#     @winner = (@cars[0].accelerate(rand(100)) < @cars[1].accelerate(rand(100))) ? @cars[1]: @cars[0]
+#   end
+#
+#
+#
+#
+#
+# end
